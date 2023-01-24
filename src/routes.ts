485df609d2f602352516694bbeead16f99f331d0
jefersonvmoraes <jefersonvm2@hotmai.com>
2023-01-24 20:08:27 -0300

    Registro de usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { authController } from './controllers/authController';
 import { categoriesController } from './controllers/categoriesController';
 import { coursesController } from './controllers/coursesController';
 import { episodesController } from './controllers/episodesController';
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
@@ -14,5 +17,7 @@ router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
 router.get('/episodes/stream', episodesController.stream)
+
+
 
 export { router }
